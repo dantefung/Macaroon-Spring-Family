046ec80dfa9577de484b9f2a24434a866dc6ffab
fenghaolin <476400902@qq.com>
2020-03-24 20:03:43 +0800

    :tada:清楚无用的包

--- thinking-in-springboot-samples/src/main/java/com/dantefung/thinkinginspringbootsamples/main/AnnotationAttributesBootstrap.java
+++ thinking-in-springboot-samples/src/main/java/com/dantefung/thinkinginspringbootsamples/main/AnnotationAttributesBootstrap.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.reflect.AnnotatedElement;
-import java.util.LinkedHashMap;
 
 /**
  * @Title: AnnotationAttributesBootstrap
