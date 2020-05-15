f0f77d6a47b74c44922afb92075934a6f8725ec9
Dante Fung <476400902@qq.com>
2020-05-15 17:51:29 +0800

    Update SignConfig.java

--- spring-boot-mvc/spring-boot-sign-plugin/src/main/java/com/dantefung/springbootmvc/sign/SignConfig.java
+++ spring-boot-mvc/spring-boot-sign-plugin/src/main/java/com/dantefung/springbootmvc/sign/SignConfig.java
@@ -56,21 +56,7 @@ public class SignConfig {
         if (map.containsKey(appId)) {
             return map.get(appId);
         } else {
-            // 从应用中心中获取服务，restTemplate被添加了@Loadbalance
-        	/*String url = String.format("http://%s/api/appsys/getServerKeyById?appSysId=%s",
-                    "os-kernel-appsysctr-microsvc".toUpperCase(), appId);
-            ResponseEntity<String> result = restTemplate.getForEntity(url, String.class);
-            if (result.getStatusCode().is2xxSuccessful()) {
-                String body = result.getBody();
-                if (StringUtils.isNotBlank(body) && !body.equals("null")) {
-                    JSONObject jsonObject = JSONObject.parseObject(body);
-                    String publicKey = jsonObject.getString("publicKey");// 获取公钥
-                    map.put(appId, publicKey);
-                    return publicKey;
-                }
-            }
-             return null;
-            */
+      
             // 本地模拟获取接入方的公钥
 			String publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAg6TIhNb0vBBl54/GjDgHe0E/uwVmTCPSKPCAUHtxe8e75XP1aE9KbjD3/Ph4uRfjC1kwcgxowxov8yU0A5rH3a5XKwUm0zpMr4OIzTrok9ebxV2F5JYJvZ5Sx5tzQQMT97Z1o3TLEgowAIDMuJiIwVXGn4W8VZl11eeA9Ry2nQtM04cj7tHlKpHap5Uvl3W7JDg6e4jS1Te9ClrHtF59sHTv6Y1f7bKAMntha2k5UdFrbGvwSKP3/s/4D4pCa9KuHqIaFRoDwNqQDCENFoNGkDAZJMVzsG6ceGcC4nFR0zU1lFiiE0JcOUgjg25lu2btcfgk/IoUslHPlAeEs2+POwIDAQAB";
 			map.put(appId, publicKey);
