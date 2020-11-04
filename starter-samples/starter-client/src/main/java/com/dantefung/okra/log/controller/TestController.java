369eeb3fbdb12333307047c7234d9848a74bce39
fenghaolin <476400902@qq.com>
2020-11-04 21:45:06 +0800

    MDC测试

--- starter-samples/starter-client/src/main/java/com/dantefung/okra/log/controller/TestController.java
+++ starter-samples/starter-client/src/main/java/com/dantefung/okra/log/controller/TestController.java
@@ -1,16 +1,35 @@
 package com.dantefung.okra.log.controller;
 
+import com.dantefung.okra.log.annontation.LogTrace;
 import com.dantefung.okra.log.annontation.SysLog;
+import com.dantefung.okra.log.trace.MDCThreadPoolExecutor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
 	@SysLog("title is ok!")
+	@LogTrace
 	@RequestMapping("/ok")
 	public String ok() {
+		log.info("==========> enter {}.ok() method ...", this.getClass().getSimpleName());
+
+		ThreadPoolExecutor pool = new MDCThreadPoolExecutor(3, 5,
+				100, MILLISECONDS, new ArrayBlockingQueue<>(5));
+		CompletableFuture.runAsync(() ->{
+
+			log.info("===============> 子线程 ...");
+		}, pool);
 		return "ok!";
 	}
 }
