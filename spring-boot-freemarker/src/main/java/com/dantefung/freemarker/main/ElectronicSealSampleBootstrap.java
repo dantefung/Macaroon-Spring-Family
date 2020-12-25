582fc8ed2f62e82c95d1d41f3bc1f30ded9e68d9
fenghaolin <476400902@qq.com>
2020-12-25 17:58:29 +0800

    TODO: Electronic seal

---
+++ spring-boot-freemarker/src/main/java/com/dantefung/freemarker/main/ElectronicSealSampleBootstrap.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C), 2015-2020
+ * FileName: ElectronicSealSampleBootstrap
+ * Author:   DANTE FUNG
+ * Date:     2020/12/25 17:56
+ * Description: PDF加盖电子公章示例
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ * DANTE FUNG        2020/12/25 17:56   V1.0.0
+ */
+package com.dantefung.freemarker.main;
+
+/**
+ * @Title: ElectronicSealSampleBootstrap
+ * @Description: PDF加盖电子公章示例
+ * 参考: https://www.jianshu.com/p/92c6faec0427
+ * @author DANTE FUNG
+ * @date 2020/12/25 17/56
+ * @since JDK1.8
+ */
+public class ElectronicSealSampleBootstrap {
+
+	public static void main(String[] args) {
+		// TODO: PDF加盖电子公章.
+	}
+}
