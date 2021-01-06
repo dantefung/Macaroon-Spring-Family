59bfa39c2f7d6ed7aeebff38b5ccf6b4f2f09cf0
fenghaolin <476400902@qq.com>
2021-01-06 16:51:11 +0800

    :tada:spring-boot-2.0-samples

---
+++ thinking-in-springboot/spring-boot-2.0-samples/externalized-configuration-sample/src/main/java/thinking/in/spring/boot/samples/externalized/configuration/bootstrap/MapConfigurationPropertySourceBootstrap.java
@@ -0,0 +1,26 @@
+package thinking.in.spring.boot.samples.externalized.configuration.bootstrap;
+
+import org.springframework.boot.context.properties.source.ConfigurationPropertyName;
+import org.springframework.boot.context.properties.source.MapConfigurationPropertySource;
+
+/**
+ * {@link MapConfigurationPropertySource} 实例 引导类
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see MapConfigurationPropertySource
+ * @since 1.0.0
+ */
+public class MapConfigurationPropertySourceBootstrap {
+
+    public static void main(String[] args) {
+
+        MapConfigurationPropertySource propertySource = new MapConfigurationPropertySource();
+
+        propertySource.put("userName", "Mercy");
+        propertySource.put("user-id", 1);
+        propertySource.put("user_id", 1);
+
+        propertySource.stream().map(name -> name.getLastElement(ConfigurationPropertyName.Form.UNIFORM))
+                .forEach(System.out::println);
+    }
+}
