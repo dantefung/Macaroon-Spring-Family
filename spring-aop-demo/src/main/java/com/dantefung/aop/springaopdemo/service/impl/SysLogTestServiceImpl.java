97fd2c02de3d768822a63bbc7caa2ee3cc20a40e
fenghaolin <476400902@qq.com>
2021-01-14 14:28:25 +0800

    :tada:新增aop自调用的解决方案.

--- spring-aop-demo/src/main/java/com/dantefung/aop/springaopdemo/service/impl/SysLogTestServiceImpl.java
+++ spring-aop-demo/src/main/java/com/dantefung/aop/springaopdemo/service/impl/SysLogTestServiceImpl.java
@@ -14,6 +14,8 @@ import com.dantefung.aop.springaopdemo.annotation.SysLog;
 import com.dantefung.aop.springaopdemo.annotation.SysLogType;
 import com.dantefung.aop.springaopdemo.service.SysLogTestService;
 import com.dantefung.aop.springaopdemo.utils.SpringContextUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 
 /**
@@ -25,14 +27,28 @@ import org.springframework.stereotype.Service;
 
  * @since 1.0.0
  **/
+@Slf4j
 @Service
 public class SysLogTestServiceImpl implements SysLogTestService {
 
 
     @Override
     public String doBiz(String msg) {
+    	// 目标对象自调用，不会受到切面管理
+    	this.saveRenewalLog(msg);
+    	// 解决aop自调用问题
+		// 方式一
         SysLogTestService sysLogTestService = SpringContextUtils.getApplicationContext().getBean(SysLogTestService.class);
-        sysLogTestService.saveRenewalLog("msg");
+		log.info(">>>>>>>>>>>方式一:从容器中重新获取SysLogTestService的实例,{}",sysLogTestService.getClass());
+        sysLogTestService.saveRenewalLog(msg);
+        // 方式二,即使用 AopContext.currentProxy() 获取到代理对象，然后通过代理对象调用对应的方法。
+		//还有个地方需要注意，以上方式还需要将 Aspect 的 expose-proxy 设置成 true。
+		// 如果是 SpringBoot，则修改应用启动入口类的注解：
+		//@EnableAspectJAutoProxy(exposeProxy = true)
+		log.info(">>>>>>>>>>>方式二:AopContext.currentProxy()获取SysLogTestService的实例,{}",AopContext.currentProxy().getClass());
+		if (AopContext.currentProxy() instanceof SysLogTestService) {
+			((SysLogTestService)AopContext.currentProxy()).saveRenewalLog(msg);
+		}
         return "ok!";
     }
 
