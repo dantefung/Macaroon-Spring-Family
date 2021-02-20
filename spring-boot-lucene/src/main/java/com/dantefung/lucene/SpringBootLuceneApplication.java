18381d8fb049c07f75b381cd32b79ee740a88aab
Dante Fung <476400902@qq.com>
2021-02-21 00:21:37 +0800

    feat: spring boot lucene hello word sample
    迈出lucene的第一步
    
    Signed-off-by: dantefung <476400902@qq.com>

---
+++ spring-boot-lucene/src/main/java/com/dantefung/lucene/SpringBootLuceneApplication.java
@@ -0,0 +1,13 @@
+package com.dantefung.lucene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLuceneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLuceneApplication.class, args);
+	}
+
+}
---
+++ spring-boot-lucene/src/test/java/com/dantefung/lucene/SpringBootLuceneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dantefung.lucene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLuceneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
