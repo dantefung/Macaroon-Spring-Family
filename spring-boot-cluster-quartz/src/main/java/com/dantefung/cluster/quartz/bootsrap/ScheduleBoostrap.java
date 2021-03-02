bdbb25f5ec10021929185c24d8824cbe1d509484
fenghaolin <476400902@qq.com>
2021-03-02 10:03:09 +0800

    :tada:修改事务项目的名称

---
+++ spring-boot-cluster-quartz/src/main/java/com/dantefung/cluster/quartz/bootsrap/ScheduleBoostrap.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C), 2015-2020
+ * FileName: ScheduleBoostrap
+ * Author:   DANTE FUNG
+ * Date:     2021/2/22 14:19
+ * Description: quartz schedule sample
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ * DANTE FUNG        2021/2/22 14:19   V1.0.0
+ */
+package com.dantefung.cluster.quartz.bootsrap;
+
+/**
+ * @Title: ScheduleBoostrap
+ * @Description: quartz schedule sample
+ * @author DANTE FUNG
+ * @date 2021/02/22 14/19
+ * @since JDK1.8
+ */
+public class ScheduleBoostrap {
+}
